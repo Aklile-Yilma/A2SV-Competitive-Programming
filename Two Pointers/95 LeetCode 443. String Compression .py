4e211e5352476da7fc5866305b527e911a65978d
Aklile-Yilma <aklileyilma@gmail.com>
2022-10-03 23:20:12 +0300

    added comment

--- Two Pointers/95 LeetCode 443. String Compression .py
+++ Two Pointers/95 LeetCode 443. String Compression .py
@@ -1,5 +1,3 @@
-
-from typing import *
 class Solution:
     def compress(self, chars: List[str]) -> int:
 
@@ -9,30 +7,33 @@ class Solution:
         while left < len(chars):
             count = 1
 
+            # move right pointer while the right value is equal to initial left value
             while right < len(chars) and chars[left] == chars[right]:
                 # print(chars[right])
                 count += 1
                 right += 1
 
-
+            #replace everything upto but not including right
             chars[left:right] = chars[left]
             
+            # if count = 1 no number is inserted
             if(count != 1):
-                num = left
+                num = left 
                 count = f"{count}"
                 if(int(count) >= 10):
                     for char in count:
-                        left+=1
-                        chars.insert(left, char)
+                        num+=1
+                        chars.insert(num, char)
                 else:
                     chars.insert(left + 1, count)
-                left = num + 1 + len(count) 
+                # move the left pointer one step and length of count because they have been inserted
+                left = left + 1 + len(count)
             else:
                 left = right 
             
             right = left + 1
             
-        return chars
+        
             
 
 s = Solution()
