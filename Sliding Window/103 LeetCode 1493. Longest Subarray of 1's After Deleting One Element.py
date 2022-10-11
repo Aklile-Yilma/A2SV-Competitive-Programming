243e5c26513dcecd33c6bd6b9908a2c1497648e8
Aklile-Yilma <aklileyilma@gmail.com>
2022-10-12 00:02:53 +0300

    content-added

--- Sliding Window/103 LeetCode 1493. Longest Subarray of 1's After Deleting One Element.py
+++ Sliding Window/103 LeetCode 1493. Longest Subarray of 1's After Deleting One Element.py
@@ -1,1 +1,31 @@
- 
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+
+        left = 0
+        result, total = 0, 0
+        #determines the number of 0
+        k = 0
+
+        for right,num in enumerate(nums):
+
+            # increamenting total by 1 if num is 1
+            total += num
+
+            # decreamenting k by 1 if num is 0
+            k += (num - 1)
+
+
+            if(k <= -2):
+                while nums[left] and nums[left] != 0:
+                    # decreamenting one because we are removing 1's from the window
+                    total -=1
+                    left += 1
+
+                # decreament total if left most window was zero
+                left += 1
+                k += 1
+
+
+            result = max(result, total)
+
+        return result if k != 0 else result - 1
