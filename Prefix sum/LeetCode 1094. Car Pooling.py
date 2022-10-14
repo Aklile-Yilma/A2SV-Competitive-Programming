34d0fa605e31e7b6a1def85e2a3446d3a8f53d4a
Aklile-Yilma <aklileyilma@gmail.com>
2022-10-14 19:52:08 +0300

    108

---
+++ Prefix sum/LeetCode 1094. Car Pooling.py
@@ -0,0 +1,19 @@
+ 
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        last_drop=-1
+        for i in trips:
+            last_drop=max(last_drop,i[2])
+
+        events=[0]*(last_drop+1)
+        for pas,st,en in trips:
+            events[st]+=pas
+            events[en]-=pas
+        if events[0]>capacity:
+            return False
+        for i in range(1,len(events)):
+            events[i]=events[i]+events[i-1]
+            if events[i]>capacity:
+                return False
+        return True
+
