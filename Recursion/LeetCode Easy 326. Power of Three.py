764eb8d7990ff1e57b68532e3ab9546bf7f0cfdc
Aklile-Yilma <aklileyilma@gmail.com>
2022-10-28 16:30:05 +0300

    oct 28

---
+++ Recursion/LeetCode Easy 326. Power of Three.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+
+        # base case: when n == 1 that means that 3^0 exists
+        if(n == 3 or n == 1):
+            return True
+        # if the current n is less than 3 that means it doesn't have any x such that 3^x = n
+        if(n < 3):
+            return False
+        return self.isPowerOfThree(n/3)
--- Two Pointers/LeetCode 18. 4Sum.py
+++ Two Pointers/LeetCode 18. 4Sum.py
@@ -1,1 +1,43 @@
- 
+from typing import *
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+
+        nums.sort()
+
+        for first_idx, first_num in enumerate(nums):
+            # used to pick the second number other than the current number
+            for second_idx, second_num in enumerate(nums[first_idx+1:]):
+
+                # left = first_idx + second_idx + 2 because we are accessing the actual list not the sliced list and its zero indexed
+                left = first_idx + second_idx + 2
+                right = len(nums) - 1
+                # picking two pairs of numbers, one current value and another one
+                required_sum = target - first_num - second_num
+                curr_sum = 0
+
+
+                while left < right:
+                    curr_sum = nums[left] + nums[right]
+
+                    if(curr_sum == required_sum):
+                        answer = (first_num, second_num, nums[left], nums[right])
+                        if not result or answer not in result:
+                            result.append(answer)
+
+
+                    if(curr_sum >= required_sum):
+                        right -= 1
+                    else:
+                        left += 1
+
+
+        return result
+
+
+
+
+
+s = Solution()
+print(s.fourSum([1,0,-1,0,-2,2], 0))
+
+
