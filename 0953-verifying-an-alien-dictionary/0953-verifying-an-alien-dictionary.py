721a8bcd825d89d441485ed38287fe70d5f02c19
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2022-12-23 16:40:10 +0000

    Time: 43 ms (80.73%), Space: 13.9 MB (83.48%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,7 +1,58 @@
+# class Solution:
+#     def isAlienSorted(self, words: List[str], order: str) -> bool:
+        
+        
+#         left, right = 0 , 1
+        
+#         while (right < len(words)):
+            
+#             idx = 0
+#             min_word_len = min(len(words[left]), len(words[right]))
+            
+#             while (idx < min_word_len):
+            
+#                 if(idx < min_word_len and order.index(words[left][idx]) < order.index(words[right][idx])):
+#                     break
+
+#                 elif(idx < min_word_len and order.index(words[left][idx]) == order.index(words[right][idx])):
+                                        
+#                     if((idx == min_word_len - 1)):
+#                         if(len(words[left]) < len(words[right])): 
+#                             # if the word at left is equal and less than the word at right
+#                             break
+#                         elif(len(words[left]) == len(words[right])):
+#                              # if the words are completely equal        
+#                             break
+#                         else:
+#                              # the word that is at the right is less than in size and equal in order at every character at left index
+#                             return False
+#                     else:
+#                         idx += 1                    
+
+#                 elif(order.index(words[left][idx]) > order.index(words[right][idx])): 
+#                     return False
+
+#                 else:
+#                     # the word that is at the right is less than in size and equal in order at every character at left index
+#                     return False
+            
+#             left += 1
+#             right += 1
+                
+#         return True
+                
+            
+                
+            
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
         
+        alienOrder = {}
         
+        # map each char in the alien dictionary to its position
+        for index, char in enumerate(order):
+            alienOrder[char] = index
+            
         
         left, right = 0 , 1
         
@@ -12,18 +63,14 @@ class Solution:
             
             while (idx < min_word_len):
             
-                if(idx < min_word_len and order.index(words[left][idx]) < order.index(words[right][idx])):
-                    # left += 1
-                    # right += 1
+                if(idx < min_word_len and alienOrder[words[left][idx]] < alienOrder[words[right][idx]]):
                     break
 
-                elif(idx < min_word_len and order.index(words[left][idx]) == order.index(words[right][idx])):
+                elif(idx < min_word_len and alienOrder[words[left][idx]] == alienOrder[words[right][idx]]):
                                         
-                    # if((idx == min_word_len -1) and (len(words[left]) == len(words[right]))):
-                    #     # if the words are completely equal                         
-                    #     return True
                     if((idx == min_word_len - 1)):
                         if(len(words[left]) < len(words[right])): 
+                            # if the word at left is equal and less than the word at right
                             break
                         elif(len(words[left]) == len(words[right])):
                              # if the words are completely equal        
@@ -34,7 +81,7 @@ class Solution:
                     else:
                         idx += 1                    
 
-                elif(order.index(words[left][idx]) > order.index(words[right][idx])): 
+                elif(alienOrder[words[left][idx]] > alienOrder[words[right][idx]]): 
                     return False
 
                 else:
@@ -43,12 +90,9 @@ class Solution:
             
             left += 1
             right += 1
-                
+            
         return True
                 
-            
-                
-            
         
             
             
