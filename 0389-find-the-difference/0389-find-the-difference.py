2c65cc2027c58b5de506806312de1c351bcaae7f
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2022-12-26 14:03:33 +0000

    Time: 36 ms (89.39%), Space: 13.9 MB (31.39%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -3,5 +3,6 @@ class Solution:
 
         s_cnt, t_cnt = Counter(s), Counter(t)
         for letter in t_cnt:
+            # if their counts don't match that means it's the added letter
             if not s_cnt[letter] == t_cnt[letter]:
                 return letter
