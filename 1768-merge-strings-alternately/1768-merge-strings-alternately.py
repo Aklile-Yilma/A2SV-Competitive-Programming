3ae213667f3b39e0535b2f4b86d5c8cb365a338b
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2022-12-26 14:41:05 +0000

    Time: 35 ms (81.23%), Space: 13.7 MB (98.85%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,14 +1,17 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         
+        #create an empty list
         merged_list = ['']*((len(word1) + len(word2))*2)
         
-        print(merged_list)
+        
+        # insert characters of word1 at even indexes 
         idx = 0
         for char in word1:
             merged_list[idx] = char
             idx += 2
             
+        # insert characters of word2 at odd indexes 
         idx = 1
         for char in word2:
             merged_list[idx] = char
