eada38ebd97661c21a5f040e8076a2148ad26576
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2022-12-26 18:04:58 +0000

    Time: 748 ms (88.66%), Space: 19.4 MB (20.58%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
@@ -6,9 +6,13 @@ class Solution:
 
         for idx ,coordinate in enumerate(points):
             if(coordinate[0] == x or coordinate[1] == y):
+                
+                # calculate manhattan distance
                 curr_distance = abs(coordinate[0]- x) + abs(coordinate[1] - y)
+                # update idx in list if it's valid minimum distance
                 if(curr_distance < distance):
                     idx_in_list = idx
+                # keep track of min distance
                 distance = min(distance, curr_distance)
                 
                 
