8f63f2c9c462a69ef4b55c46cd7f0d7dad4163e5
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2022-12-27 14:35:25 +0000

    Time: 279 ms (67.05%), Space: 15.4 MB (93.19%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -8,6 +8,7 @@ class Solution:
         
         while(idx < len(nums)):
             
+            # a triangle is said to be formed if the sum two less length sides is greater than the third bigger side 
             if((idx+2) < len(nums) and (nums[idx] + nums[idx+1] > nums[idx+2])):
                 curr_perimeter = nums[idx] + nums[idx+1] + nums[idx+2]
                 largest_perimeter = max(largest_perimeter, curr_perimeter)
