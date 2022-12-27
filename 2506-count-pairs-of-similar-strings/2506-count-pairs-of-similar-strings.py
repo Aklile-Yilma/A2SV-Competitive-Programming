0cd7c5b4c31cac1a01291054208daca3dd42c6c7
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2022-12-27 15:17:40 +0000

    Time: 78 ms (87.25%), Space: 14.1 MB (12.89%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
@@ -3,12 +3,14 @@ class Solution:
         
         pairs = 0
         
+        # use set to remove duplicates
         for idx, word in enumerate(words):
             words[idx] = set(word)
             
 
         for idx1 in range(len(words)):
             for idx2 in range(idx1+1, len(words), 1):
+                # increase the number of pairs if they are equal
                 if(words[idx1] == words[idx2]):
                     pairs += 1
                           
