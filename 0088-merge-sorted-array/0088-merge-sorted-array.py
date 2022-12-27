895686d14ff7bd133d14e537dab9f69de3c285b7
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2022-12-27 14:48:51 +0000

    Time: 44 ms (77.27%), Space: 13.8 MB (99.56%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,37 +4,6 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
 
-#         idx2 = 0
-#         for idx in range(m, len(nums1), 1):
-#             if(idx2 < n):
-#                 nums1[idx] = nums2[idx2]
-#                 idx2 +=1
-
-#         nums1ptr=0
-#         nums2ptr=m
-        
-#         while(nums2ptr<len(nums1)):
-#             if nums1[nums1ptr] > nums1[nums2ptr]:
-#                 nums1[nums1ptr],nums[nums[]]
-
-
-
-
-
-        # nums1.sort()
-
-        # idx = 0 
-        # for num1, num2 in zip(nums1, nums2):
-        #     if(num2 > num1):
-        #         nums1.insert(idx, num2)
-        # idx2 = n-1
-        # last=n+m-1
-        # for idx in range(m, 0, -1):
-        #     if nums1[idx] < nums2[idx2]:
-        #         nums1[last]=nums2[idx2]
-        #         last-=1
-        #     else:
-        #         n
         
         idx2 = 0
         for idx in range(m, len(nums1), 1):
