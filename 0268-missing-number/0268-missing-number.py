b2535dd772cb14ea2e57f32a2adc1374fe720ef5
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2022-12-27 16:00:21 +0000

    Time: 148 ms (80.42%), Space: 15.5 MB (16.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,11 +1,13 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
+        # to get range of number from 0  to n
         n = len(nums) + 1
-        
         nums = set(nums)
         
+        
         for num in range(n):
+            # o(1) to check an item is in a set
             if(num not in nums):
                 return num
             
