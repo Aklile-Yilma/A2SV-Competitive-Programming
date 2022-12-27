082fe70680d7918125be1787ffa61f52f7802eda
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2022-12-27 16:45:03 +0000

    Time: 1934 ms (89.17%), Space: 68.9 MB (42.86%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -12,22 +12,26 @@ class Solution:
             player1 = match[0]
             player2 = match[1]
             
+            # add player to count.key
             if(player1 not in count.keys()):
                 count[player1] = 0
             
-                
+            #add lost amount    
             if(player2 not in count.keys()):
                 count[player2] = 1
             else:
                 count[player2] += 1     
                 
+                
         for player in count.keys():
+            # collect winners
             if(count[player] == 0):
                 winners.append(player)
             elif(count[player] == 1):
+                #collect players who lost only one match
                 lost_one_match.append(player)
                 
-        
+        #sort them in order
         winners.sort()
         lost_one_match.sort()
         
