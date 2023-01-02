30fa24d495c01ac2d32a4bd7081a47ffa31bbb93
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-02 18:59:30 +0300

    Update 4-patterns.py

--- Contests/Contest#2/4-patterns.py
+++ Contests/Contest#2/4-patterns.py
@@ -1,27 +1,30 @@
  
 n = int(input())
-
-
+ 
+ 
 answer = ''
 answer_length = 0
-
-
+ 
+ 
 for _ in range(n):
     pattern = input()
     answer_length = len(pattern)
-
+ 
     for idx, char in enumerate(pattern):
         #intial word setup
         if(len(answer) != answer_length):
             answer += char
         elif(answer[idx] == '?'):
             answer = answer[:idx] + char + answer[idx+1:]
-
+ 
         elif(answer[idx]==char or answer[idx] == '!'):
             continue
-        elif(answer[idx] != char):
+        elif(char != '?' and answer[idx] != char):
             answer = answer[:idx] + '!' + answer[idx+1:]
-
-
+    
+    
+ 
+answer = answer.replace('?', 'a')
+answer = answer.replace('!', '?')
 print(answer)
 
