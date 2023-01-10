d85724b519c5a41db3a5ebe6d6cea59d85b1063e
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-10 01:40:13 +0000

    Time: 1808 ms (87.45%), Space: 48.8 MB (61.32%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -5,7 +5,9 @@ class Solution:
         row_length = len(grid)
         col_length = len(grid[0])
         
+        # represents summation of all 1 in a row
         ones_row = [0] * row_length
+        # represents summation of all 1 in a col
         ones_col = [0] * col_length
         
         for i in range(row_length):
@@ -14,13 +16,14 @@ class Solution:
                     ones_row[i] += 1
                     ones_col[j] += 1
         
-        print(ones_row)
-        print(ones_col)
         
         diff = [[0] * col_length for _ in range(row_length)]
-        
+
+
+    
         for i in range(row_length):
             for j in range(col_length):
+                # we can find the number of zeros by subtracting from row/col length
                 diff[i][j] = ones_row[i] + ones_col[j] - (row_length - ones_row[i]) - (col_length - ones_col[j])
         
         return diff
