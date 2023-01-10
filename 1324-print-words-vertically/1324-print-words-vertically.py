b183fd43062aa3162d345fd38f82bf0ef03a4e4c
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-10 16:22:15 +0000

    Time: 28 ms (92.86%), Space: 13.9 MB (67.74%) - LeetHub

---
+++ 1324-print-words-vertically/1324-print-words-vertically.py
@@ -0,0 +1,24 @@
+class Solution:
+    def printVertically(self, s: str) -> List[str]:
+        
+        matrix = [[letter for letter in word] for word in  s.split()]
+        #create the maximum column possible
+        col_length = max(len(word) for word in matrix)
+        row_length = len(matrix)
+        # generate a transposed matrix
+        vertical_words = [["" for _ in range(row_length)] for _ in range(col_length)]
+
+        # print(vertical_words)
+        # print(matrix)
+        for row_idx in range(row_length):
+            for col_idx in range(col_length):
+                if col_idx < len(matrix[row_idx]):
+                    vertical_words[col_idx][row_idx] = matrix[row_idx][col_idx]
+                else:
+                    vertical_words[col_idx][row_idx] = " "
+                        
+       # join and remove trailing spaces 
+        vertical_words = ["".join(letters).rstrip() for letters in vertical_words]
+        
+        return vertical_words
+        
