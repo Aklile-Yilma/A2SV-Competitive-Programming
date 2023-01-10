8c2a34545445151d3ce1c6262091d7ea6c384a6b
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-10 02:37:45 +0000

    Time: 195 ms (89.73%), Space: 17.7 MB (46.55%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -2,41 +2,45 @@ class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
 
             
-        rows = len(mat)
-        cols = len(mat[0])
+        row_length = len(mat)
+        col_length = len(mat[0])
         res = []
         cur_row = cur_col = 0
         
         going_up = True
         
-        while len(res) < rows * cols:
+        while len(res) < row_length * col_length:
             if going_up:
-                while cur_row >= 0 and cur_col < cols:
+                while cur_row >= 0 and cur_col < col_length:
                     res.append(mat[cur_row][cur_col])
                     
-                    cur_row-= 1
-                    cur_col+= 1
+                    # going upper
+                    cur_row -= 1
+                    cur_col += 1
                 
-                # when we are out of bounds 
-                if cur_col == cols:
-                    cur_col-= 1
-                    cur_row+= 2
+                # when we are out of bounds at the highest edge we need to move down twice and turn left
+                if cur_col == col_length:
+                    cur_col -= 1
+                    cur_row += 2
                 else:
+                    # turn left
                     cur_row += 1
                 
                 going_up = False
             else:
-        
-                while cur_row < rows and cur_col >= 0:
+                while cur_row < row_length and cur_col >= 0:
                     res.append(mat[cur_row][cur_col])
                     
-                    cur_col-= 1
+                    # going down
+                    cur_col -= 1
                     cur_row += 1
-        
-                if cur_row == rows:
-                    cur_col+= 2
-                    cur_row-= 1
+                    
+                # when we are out of bounds at the highest edge we need to move up twice and turn right            
+                if cur_row == row_length:
+                    cur_col += 2
+                    cur_row -= 1
                 else:
+                    # turn right
                     cur_col += 1
                 
                 going_up = True
