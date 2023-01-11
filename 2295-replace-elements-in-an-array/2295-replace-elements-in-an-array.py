4c8591f95251402046ce1701a7fc94ddd1b7e0c6
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-11 11:57:36 +0000

    Time: 1460 ms (63.42%), Space: 69.3 MB (57.05%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.py
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.py
@@ -2,14 +2,16 @@ class Solution:
     def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
         
         nums_map = {}
-        # nums = [None] * len(nums)
         for idx, num in enumerate(nums):
             nums_map[num] = idx
                     
         for num, replacement in operations:
             idx = nums_map[num]
+            #remove the (key,value) pairs b/c they are no longer in the dict
             del nums_map[num]
+            #replace the num in nums
             nums[idx] = replacement
+            #update the dict
             nums_map[replacement] = idx
             
         return nums
