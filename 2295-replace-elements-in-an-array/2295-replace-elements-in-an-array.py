1a7b853696a789e6cb4b3fb873a45348e98eed29
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-11 11:51:39 +0000

    Time: 1391 ms (88.25%), Space: 73.7 MB (5.03%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.py
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.py
@@ -4,17 +4,12 @@ class Solution:
         nums_map = {}
         for idx, num in enumerate(nums):
             nums_map[num] = idx
-            
-            
-        print(nums_map)
-        
+                    
         for num, replacement in operations:
             idx = nums_map[num]
             del nums_map[num]
             nums_map[replacement] = idx
             
-        print(nums_map)
-        
         # sort with values and return only the keys
         return dict(sorted(nums_map.items(), key=lambda x: x[1])).keys()
             
