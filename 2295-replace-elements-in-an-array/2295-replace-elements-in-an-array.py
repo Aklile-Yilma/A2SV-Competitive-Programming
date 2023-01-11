19cd0c53742f5edc875c5b978bb62b44d9882eb0
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-11 11:55:54 +0000

    Time: 1453 ms (66.11%), Space: 70 MB (28.52%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.py
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.py
@@ -2,16 +2,19 @@ class Solution:
     def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
         
         nums_map = {}
+        # nums = [None] * len(nums)
         for idx, num in enumerate(nums):
             nums_map[num] = idx
                     
         for num, replacement in operations:
             idx = nums_map[num]
             del nums_map[num]
+            nums[idx] = replacement
             nums_map[replacement] = idx
             
         # sort with values and return only the keys
-        return dict(sorted(nums_map.items(), key=lambda x: x[1])).keys()
+        # return dict(sorted(nums_map.items(), key=lambda x: x[1])).keys()
+        return nums
             
             
             
