014f877d1f955c289fe874a92682795bd98e8e99
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-12 01:30:20 +0000

    Time: 1621 ms (5.64%), Space: 13.8 MB (77.22%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -7,17 +7,20 @@ class Solution:
         temp_k = k
 
         while num_players > 1:
+            #if it's not delelted
             if players[index-1]:
                 temp_k -= 1
                 if temp_k == 0:
+                    #delete player and reset k
                     players[index-1] = None
                     temp_k = k
                     num_players -= 1
-
+            
             index += 1
             if index > n:
                 index = 1
 
+        #find the remaining player and return it
         for num in players:
             if num:
                 return num
