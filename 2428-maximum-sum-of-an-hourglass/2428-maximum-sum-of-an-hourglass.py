7ef7d8cc97703f9652339cbb163b3c41a22371ca
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-13 14:35:17 +0000

    Time: 270 ms (93.06%), Space: 17.2 MB (26.01%) - LeetHub

--- 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
@@ -4,11 +4,22 @@ from typing import List
 class Solution:
     def maxSum(self, grid: List[List[int]]) -> int:
         row_length, col_length  = len(grid), len(grid[0])
-        return max(
-            self.get_hourglass(grid, i, j)
-            for i in range(1, row_length - 1)
-            for j in range(1, col_length - 1)
-        )
+        
+        max_sum = 0
+        
+        for i in range(1, row_length-1):
+            for j in range(1, col_length-1):
+                curr_sum = self.get_hourglass(grid, i, j)
+                
+                max_sum = max(max_sum, curr_sum)
+        
+        
+        return max_sum
+#         return max(
+#             self.get_hourglass(grid, i, j)
+#             for i in range(1, row_length - 1)
+#             for j in range(1, col_length - 1)
+#         )
 
 
     def get_hourglass(self, grid: List[List[int]], i: int, j: int) -> int:
