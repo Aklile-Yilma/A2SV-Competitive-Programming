d3fbbaeeb273d2f69f55128b1f20ca07ba6a3987
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-13 15:28:23 +0000

    Time: 169 ms (80.67%), Space: 14.6 MB (59.05%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -21,9 +21,19 @@ class Solution:
         return word == ''.join(sorted(word))
                 
                 
-                
-                
-                
+#     more efficient
+# class Solution:
+#     def minDeletionSize(self, strs: List[str]) -> int:
+#         m, n = len(strs), len(strs[0])
+#         count = 0
+        
+#         for i in range(n):
+#             for j in range(1,m):
+#                 if strs[j][i] < strs[j-1][i]:
+#                     count += 1
+#                     break
+        
+#         return count
         
         
     
