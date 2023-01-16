6d4cc8c724294c36c614fb62c22a115144fb95fb
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-16 16:56:20 +0000

    Time: 92 ms (67.85%), Space: 13.9 MB (72.91%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -4,6 +4,7 @@ class Solution:
         row_length = len(matrix)
         col_length = len(matrix[0])
         
+        # for each number check to see if the next diagonal number is equal to it or not
         for row_idx in range(row_length-1):
             for col_idx in range(col_length-1):
                 
