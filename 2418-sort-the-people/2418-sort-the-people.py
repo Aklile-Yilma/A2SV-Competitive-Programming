1f24b41d12626fd1290a8c7ed9be615ee990480b
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-17 16:07:36 +0000

    Time: 1837 ms (5.03%), Space: 14.4 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,37 +1,38 @@
-# class Solution:
-#     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        
-#         for first_idx in range(len(names)):
-#             for idx in range(len(names[first_idx:])):
-#                 second_idx = first_idx + idx + 1
-                
-#                 if(second_idx >= len(names)):
-#                     break
-                    
-#                 if(heights[first_idx] < heights[second_idx]):
-#                     names[first_idx], names[second_idx] = names[second_idx], names[first_idx]
-#                     heights[first_idx], heights[second_idx] = heights[second_idx], heights[first_idx]  
-                        
-#         return names
-                
 class Solution:
-    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:      
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
-        for idx in range(len(names)):
-            is_swapped = False
-            for idx2 in range(len(names)-1):
-                first_idx = idx2
-                second_idx = idx2 + 1
+        for first_idx in range(len(names)-1):
+            for idx in range(first_idx, len(names)):
+                second_idx = idx 
                 
-                if(heights[idx2] < heights[idx2+1]):
+                if(second_idx >= len(names)):
+                    break
+                    
+                if(heights[first_idx] < heights[second_idx]):
                     names[first_idx], names[second_idx] = names[second_idx], names[first_idx]
                     heights[first_idx], heights[second_idx] = heights[second_idx], heights[first_idx]  
-                    is_swapped = True
-                    
-            if not is_swapped:
-                break
-                
+                        
         return names
+    
+# Bubble sort
+# class Solution:
+#     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:      
+        
+#         for idx in range(len(names)):
+#             is_swapped = False
+#             for idx2 in range(len(names)-1):
+#                 first_idx = idx2
+#                 second_idx = idx2 + 1
+                
+#                 if(heights[idx2] < heights[idx2+1]):
+#                     names[first_idx], names[second_idx] = names[second_idx], names[first_idx]
+#                     heights[first_idx], heights[second_idx] = heights[second_idx], heights[first_idx]  
+#                     is_swapped = True
+                    
+#             if not is_swapped:
+#                 break
+                
+#         return names
                     
 
                 
