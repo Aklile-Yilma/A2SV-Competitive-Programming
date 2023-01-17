7c5fb7a2f48df41af52c8a511f7093a65d77c4c1
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-17 16:39:54 +0000

    Time: 1249 ms (6.74%), Space: 14.3 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,16 +1,37 @@
-# Select Sort
+# Insertion sort
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
-        for first_idx in range(len(names)-1):
-            for idx in range(first_idx, len(names)):
-                second_idx = idx 
-                    
-                if(heights[first_idx] < heights[second_idx]):
-                    names[first_idx], names[second_idx] = names[second_idx], names[first_idx]
-                    heights[first_idx], heights[second_idx] = heights[second_idx], heights[first_idx]  
-                        
+        
+        for idx in range(1, len(names)):
+            first_idx = idx
+            second_idx = first_idx - 1
+            while first_idx > -1 and second_idx > -1 and heights[first_idx] > heights[second_idx]:
+                names[first_idx], names[second_idx] = names[second_idx], names[first_idx]
+                heights[first_idx], heights[second_idx] = heights[second_idx], heights[first_idx]  
+                second_idx -= 1
+                first_idx -= 1
+                
+                
         return names
+                
+
+
+
+
+# # Select Sort
+# class Solution:
+#     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        
+#         for first_idx in range(len(names)-1):
+#             for idx in range(first_idx, len(names)):
+#                 second_idx = idx 
+                    
+#                 if(heights[first_idx] < heights[second_idx]):
+#                     names[first_idx], names[second_idx] = names[second_idx], names[first_idx]
+#                     heights[first_idx], heights[second_idx] = heights[second_idx], heights[first_idx]  
+                        
+#         return names
     
 # Bubble sort
 # class Solution:
@@ -23,8 +44,8 @@ class Solution:
 #                 second_idx = idx2 + 1
                 
 #                 if(heights[idx2] < heights[idx2+1]):
-#                     names[first_idx], names[second_idx] = names[second_idx], names[first_idx]
-#                     heights[first_idx], heights[second_idx] = heights[second_idx], heights[first_idx]  
+                    # names[first_idx], names[second_idx] = names[second_idx], names[first_idx]
+                    # heights[first_idx], heights[second_idx] = heights[second_idx], heights[first_idx]  
 #                     is_swapped = True
                     
 #             if not is_swapped:
