2a2df5b825a14733f0782e49afac220279a4a0ce
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-19 20:44:24 +0000

    Time: 91 ms (79.15%), Space: 14.6 MB (98.84%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.py
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -1,5 +1,6 @@
 class Solution:
-    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:        
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        
         row_length = len(mat)
         col_length = len(mat[0])
         
@@ -7,15 +8,37 @@ class Solution:
             return mat
         
         new_mat = [['' for i in range(c)] for i in range(r)]
-        
+        flatten_list = [col for row in mat for col in row]        
         idx = 0
-        for row_idx in range(row_length):
-            for col_idx in range(col_length):
-                new_mat[idx//c][idx%c] = mat[row_idx][col_idx]
+        for row_idx in range(r):
+            for col_idx in range(c):
+                new_mat[row_idx][col_idx] = flatten_list[idx]
                 idx += 1
+                    
+        return new_mat
+    
+# class Solution:
+#     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:  
+    
+#         row_length = len(mat)
+#         col_length = len(mat[0])
+        
+#         if row_length * col_length != r * c:
+#             return mat
+        
+#         new_mat = [['' for i in range(c)] for i in range(r)]
+        
+#         idx = 0
+#         for row_idx in range(row_length):
+#             for col_idx in range(col_length):
+#                 new_mat[idx//c][idx%c] = mat[row_idx][col_idx]
+#                 idx += 1
 
             
-        return new_mat 
+#         return new_mat 
+        
+        
+        
         
         
         
