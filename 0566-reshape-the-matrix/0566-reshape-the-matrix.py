2eaf3b25a1e9bc7a14931afe4f03aa9ba108e5ca
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-19 20:35:36 +0000

    Time: 94 ms (69.01%), Space: 14.7 MB (75.45%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:        
+        row_length = len(mat)
+        col_length = len(mat[0])
+        
+        if row_length * col_length != r * c:
+            return mat
+        
+        new_mat = [['' for i in range(c)] for i in range(r)]
+        flatten_list = [col for row in mat for col in row]
+        isLegal = True
+        
+        idx = 0
+        for row_idx in range(row_length):
+            for col_idx in range(col_length):
+                new_mat[idx//c][idx%c] = mat[row_idx][col_idx]
+                idx += 1
+
+            
+        return new_mat 
+        
+        
+        
