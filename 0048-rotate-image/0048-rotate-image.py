409651f2dcc20af36fdaed74a621bc4e5214eb10
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-21 18:04:59 +0000

    Time: 73 ms (18.08%), Space: 14 MB (25.57%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -4,17 +4,32 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         
+#         n = len(matrix)
+        
+# 		# iterate through matrix
+#         for i in range(n):
+#             for j in range(i,n):
+			
+# 			    # transpose the matrix, turning rows into columns and vice versa
+#                 matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+			
+# 			# reverse the resulting rows
+#             matrix[i].reverse()
+            
+            
+        # list(zip(*matrix))
+        
+        matrix.reverse()
         n = len(matrix)
         
-		# iterate through matrix
-        for i in range(n):
-            for j in range(i,n):
-			
-			    # transpose the matrix, turning rows into columns and vice versa
-                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
-			
-			# reverse the resulting rows
-            matrix[i].reverse()
+        for row_idx in range(n):        
+            for col_idx in range(row_idx, n):
+                matrix[row_idx][col_idx],matrix[col_idx][row_idx] = matrix[col_idx][row_idx], matrix[row_idx][col_idx]
+                print(matrix)
+                
+                
+        
+        
 
         
         
