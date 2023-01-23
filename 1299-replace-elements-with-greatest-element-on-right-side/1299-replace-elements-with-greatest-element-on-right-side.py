b7d37d720a319791f77c4de96a1f95e8ac8eec2c
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-23 03:13:52 +0000

    Time: 1069 ms (36.67%), Space: 15.5 MB (68.32%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -2,7 +2,7 @@ class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
         greater = -1
         for idx in range(len(arr)-1, -1, -1):
-            
+            #swap
             greater,arr[idx] = arr[idx], greater    
             if greater < arr[idx]:
                 greater = arr[idx]
