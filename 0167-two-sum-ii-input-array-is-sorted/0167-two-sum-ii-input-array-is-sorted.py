db101037b89ae5b9e9328f34d5a701dbcfde744f
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-24 15:57:25 +0000

    Time: 209 ms (33.49%), Space: 14.9 MB (37.22%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -3,6 +3,7 @@ class Solution:
         
         left = 0
         right = len(numbers) - 1
+        # since the array is sorted we can use colliding two pointer to get the indices
         
         while left < right:
             
