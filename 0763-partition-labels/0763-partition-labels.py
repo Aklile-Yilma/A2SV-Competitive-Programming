6e64289791c5c70f4aa2ad4abfb165907ba80b7e
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-26 15:07:56 +0000

    Time: 46 ms (58.75%), Space: 13.8 MB (97.36%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,36 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+  
+        # dictionary containing {letter: last_index_seen}
+        last_index = {}
+    
+        # create our hash map/dict
+        for idx, letter in enumerate(s):
+            last_index[letter] = idx
+            
+            
+    
+        size, end, result = 0, 0, []
+        
+        for start, letter in enumerate(s):
+            
+            size +=1
+            # keep track of the max index of a letter in our partition
+            end = max(end, last_index[letter])
+            
+            # if our current index is known to be the last index of letter
+            if (end == start):
+                result.append(size)
+                size = 0
+                
+        return result 
+            
+            
+            
+            
+            
+            
+
+
+# TIME-COMPLEXITY: O(n)
+# SPACE-COMPLEXITY: O(26) == 0(1)
