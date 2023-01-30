8096b190829a46e1c1765207c109b7238a52b9bc
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-01-30 00:09:51 +0000

    Time: 94 ms (82.76%), Space: 14.1 MB (84.24%) - LeetHub

--- 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
@@ -6,6 +6,8 @@ class Solution:
         
         while first_ptr < len(word1) and second_ptr < len(word2):
             
+            # if two characters are the same pick the lexicographically largest merge
+            # that is why we compare with the split operator[:]
             if word1[first_ptr:] > word2[second_ptr:]:
                 merge.append(word1[first_ptr])
                 first_ptr += 1
