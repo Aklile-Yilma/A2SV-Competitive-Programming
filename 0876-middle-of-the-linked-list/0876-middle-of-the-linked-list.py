205b853ff9f480fd5db2132cb28619f1edaade57
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-02-02 02:13:29 +0000

    Time: 36 ms (63.51%), Space: 13.9 MB (9.55%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -4,7 +4,6 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
         
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
