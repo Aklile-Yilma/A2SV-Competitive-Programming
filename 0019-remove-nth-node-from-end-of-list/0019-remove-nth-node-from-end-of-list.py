5a106b3f0c56172ba1d65d94626af48747a0e6ac
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-02-04 09:45:11 +0000

    Time: 39 ms (49.84%), Space: 13.8 MB (97.65%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -6,28 +6,49 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         
+        dummy = ListNode(0, head)
+        slow = dummy
+        fast = head
         
-        size = 0
-        curr = head
+        # slow and fast pointer have a fixed window size of (N) PLUS ONE because of dummy node
+        while n>0 and fast:
+            fast = fast.next
+            n-=1
+            
+        while fast:
+            slow = slow.next
+            fast = fast.next
+        #delete the node
+        slow.next = slow.next.next
+            
+        return dummy.next
         
-        while curr:
-            size += 1
-            curr = curr.next
-            
+
+# class Solution:
+#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         
-        if size - n == 0:
-            head = head.next
-            return head
+        
+#         size = 0
+#         curr = head
+#         # find size of linked list
+#         while curr:
+#             size += 1
+#             curr = curr.next
             
-        curr = head
-        idx = 1
-        while curr:
-            if size - n == idx:
-                curr.next = curr.next.next
-                
-            idx += 1
-            curr = curr.next
+#         # edge case: remove the first node
+#         if size - n == 0:
+#             head = head.next
+#             return head
             
-        return head
+#         curr = head
+#         idx = 1
+#         while curr:
+#             # skip nth node from end of list
+#             if size - n == idx:
+#                 curr.next = curr.next.next
+#             idx += 1
+#             curr = curr.next
+            
+#         return head
             
                 
