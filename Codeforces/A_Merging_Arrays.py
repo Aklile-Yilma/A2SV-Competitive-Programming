a632dd766a80b6950712d6346d0b74df2f4631bd
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-02-10 19:38:35 +0300

    Update A_Merging_Arrays.py

--- Codeforces/A_Merging_Arrays.py
+++ Codeforces/A_Merging_Arrays.py
@@ -3,10 +3,10 @@ first_arr = list(map(int, input().split()))
 second_arr = list(map(int, input().split()))
 size = n + m
 third_arr = []
-
+ 
 first_pointer, second_pointer = 0, 0
-
-
+ 
+ 
 while first_pointer < n and second_pointer < m:
     
     if first_arr[first_pointer] <= second_arr[second_pointer]:
@@ -14,12 +14,12 @@ while first_pointer < n and second_pointer < m:
         first_pointer += 1
     else:
         third_arr.append(second_arr[second_pointer])
-
+        second_pointer += 1        
+ 
 third_arr += first_arr[first_pointer:]
 third_arr += second_arr[second_pointer:]
         
 print(*third_arr)
-        
         
         
     
