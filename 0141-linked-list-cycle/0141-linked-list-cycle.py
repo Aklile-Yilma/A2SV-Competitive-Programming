d29a9e612fd3a64cac7887b141ef35992e52eeb5
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-02-10 02:39:17 +0000

    Time: 56 ms (75.52%), Space: 17.6 MB (26.89%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -3,16 +3,34 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:    
-        node_set = set()
-        while(head):
-            if head in node_set:
+        
+        #Floyd's Tortoise and Hare
+        tortoise = head
+        hare = head
+        
+        #check cycle
+        while hare and hare.next:
+            tortoise = tortoise.next
+            hare = hare.next.next
+            
+            if tortoise == hare:
                 return True
-            node_set.add(head)
-            head = head.next
+        #reached null so no cycle    
         return False
+            
+
+
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:    
+#         node_set = set()
+#         while(head):
+#             if head in node_set:
+#                 return True
+#             node_set.add(head)
+#             head = head.next
+#         return False
 
             
             
