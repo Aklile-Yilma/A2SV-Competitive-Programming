0eecea331f2236b5a523ad8f1b6b5d7f01b19666
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-02-11 07:56:51 +0000

    Time: 52 ms (75.07%), Space: 17.2 MB (91.71%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #floyd's algorithm
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                slow = head
+                #find cycle node
+                while slow is not fast:
+                    fast = fast.next
+                    slow = slow.next
+                return slow
+            
+        return None
+        
