df59133802851e6e1a22c1ea35146582e49e0d7a
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-02-12 07:35:30 +0000

    Time: 867 ms (46.15%), Space: 29.2 MB (23.93%) - LeetHub

---
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
@@ -0,0 +1,26 @@
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        
+        players_ptr = 0
+        trainers_ptr = 0
+        max_match = 0
+        
+        players.sort()
+        trainers.sort()
+        while players_ptr < len(players) and trainers_ptr < len(trainers):
+            
+            if players[players_ptr] <= trainers[trainers_ptr]:
+                max_match += 1
+                players_ptr += 1
+                trainers_ptr += 1
+            else:
+                trainers_ptr += 1
+                
+        return max_match
+                
+            
+                
+            
+            
+            
+            
