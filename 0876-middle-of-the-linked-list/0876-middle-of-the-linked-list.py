3b53bd154a31b4ceae9c696be8de2bf42669c4c8
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-02-20 15:52:33 +0300

    Time: 24 ms (96.27%), Space: 14 MB (7.18%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -3,15 +3,36 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+#         slow = head
+#         fast = head
+        
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+            
+#         return slow
+    
+
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        slow = head
-        fast = head
         
-        while fast and fast.next:
+        size = 0
+        curr = head
+        while curr:
+            curr = curr.next
+            size += 1    
+        middle = math.ceil(size // 2)
+        
+        slow = head
+        curr_idx = 0
+        
+        while curr_idx < middle:
             slow = slow.next
-            fast = fast.next.next
-            
+            curr_idx += 1
         return slow
         
+            
