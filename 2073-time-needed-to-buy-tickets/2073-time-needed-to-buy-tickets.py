6a1bdcc13f3f4ead087f208482cd15dc765b642e
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-02-25 21:50:18 +0300

    Time: 73 ms (10.64%), Space: 13.9 MB (53.58%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -5,12 +5,34 @@ class Solution:
         
         while tickets[k] != 0:
             
-            for idx in range(len(tickets)):
-                if tickets[idx] != 0:
-                    tickets[idx] -= 1
-                    time += 1
-                
-                if tickets[k] == 0:
-                    break
-                
+            if tickets[0] != 0:
+                tickets[0] -= 1
+                time += 1
+            
+            front = tickets.pop(0)
+            tickets.append(front)
+            
+            if k > 0:
+                k -= 1
+            else:
+                k = len(tickets) - 1
+            
         return time
+       
+
+# class Solution:
+#     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        
+#         time = 0
+        
+#         while tickets[k] != 0:
+            
+#             for idx in range(len(tickets)):
+#                 if tickets[idx] != 0:
+#                     tickets[idx] -= 1
+#                     time += 1
+                
+#                 if tickets[k] == 0:
+#                     break
+                
+#         return time
