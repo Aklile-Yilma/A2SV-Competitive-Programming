385554d3837c7f099d513a6432d861baee8832cb
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-02-28 10:42:29 +0300

    Time: 35 ms (59.67%), Space: 13.7 MB (99.83%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,16 +1,34 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        target = n
-        def helper(curr_num, target):
-            #base case
-            if curr_num == target:
-                return True
-            if target < curr_num:
-                return False
+        
+        # base case: when n reaches 4 or 1
+        if n == 1:
+            return True
+        
+        if n < 1:
+            return False
+        
+        if n%4 != 0:
+            return False
+        
+        
+        # Recusive case: call function with the next n/4 
+        return self.isPowerOfFour(n//4)
+        
+
+# class Solution:
+#     def isPowerOfFour(self, n: int) -> bool:
+#         target = n
+#         def helper(curr_num, target):
+#             #base case
+#             if curr_num == target:
+#                 return True
+#             if target < curr_num:
+#                 return False
             
-            return helper( curr_num * 4 , target)
+#             return helper( curr_num * 4 , target)
             
-        return helper(1, target)
+#         return helper(1, target)
 
 
 # class Solution:
