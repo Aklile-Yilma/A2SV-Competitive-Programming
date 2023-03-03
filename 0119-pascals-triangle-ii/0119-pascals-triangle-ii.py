1644925c6b028e29ddbc4e65533d41ad015cb059
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-03-03 17:49:43 +0300

    Time: 40 ms (21.92%), Space: 13.8 MB (51.27%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        
+        if rowIndex == 0:
+            return [1]
+        
+        before = self.getRow(rowIndex - 1)
+        newArr = []        
+        for idx in range(len(before)-1):
+            #sum the new array
+            newArr.append(before[idx] + before[idx+1])
+        newArr.insert(0, 1)
+        newArr.append(1)
+        
+        return newArr
