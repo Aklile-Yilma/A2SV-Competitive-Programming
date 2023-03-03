247bc7d40f1d39fecd48fb73a3b50971b3a4db8e
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-03-03 22:18:05 +0300

    Time: 214 ms (31.07%), Space: 43.3 MB (5.86%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,11 +4,14 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         
-        left = 0
-        right = len(s)-1
+        def helper(start, end):
+            
+            if start > end:
+                return 
+            
+            s[start], s[end] = s[end], s[start]
+            return helper(start+1, end-1)
         
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left += 1
-            right -= 1
-        
+        return helper(0, len(s)-1)
+                
+            
