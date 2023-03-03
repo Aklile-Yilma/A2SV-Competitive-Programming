343248bfdf5f6cc8b3b1afabda217a4aab7f5281
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-03-03 22:56:35 +0300

    Time: 45 ms (29.43%), Space: 13.9 MB (23.20%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -6,26 +6,52 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
-        list3 = ListNode()
-        list3_head = list3
-        
-        while list1 and list2:
+        def merge(merged_list, list1, list2):
+            
+            if not list1 or not list2:
+                if list1:
+                    merged_list.next = list1
+                    return 
+                elif list2:
+                    merged_list.next = list2
+                    return 
+                return None
             
             if list1.val <= list2.val:
-                list3.next = list1
-                list1 = list1.next
+                merged_list.next = ListNode(list1.val)
+                merge(merged_list.next, list1.next, list2)
             else:
-                list3.next = list2
-                list2 = list2.next
-                
-            list3 = list3.next
+                merged_list.next = ListNode(list2.val)
+                merge(merged_list.next, list1, list2.next)
             
-        if list1:
-            list3.next = list1
+        dummy = ListNode()
+        dummy_head = dummy
+        merge(dummy, list1, list2)
         
-        if list2:
-            list3.next = list2
+        return dummy_head.next
             
-        return list3_head.next
+            
+        
+#         list3 = ListNode()
+#         list3_head = list3
+        
+#         while list1 and list2:
+            
+#             if list1.val <= list2.val:
+#                 list3.next = list1
+#                 list1 = list1.next
+#             else:
+#                 list3.next = list2
+#                 list2 = list2.next
+                
+#             list3 = list3.next
+            
+#         if list1:
+#             list3.next = list1
+        
+#         if list2:
+#             list3.next = list2
+            
+#         return list3_head.next
             
         
