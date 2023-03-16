dfc6db6db4169efbe35fe1c74fac18e1b7d17463
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-03-16 11:31:35 +0300

    Time: 38 ms (49.39%), Space: 14.1 MB (27.58%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,14 +2,9 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         
         curr_sub = []
-        curr_subset = set()
-        subsets = []
+        subsets = [[]]
         
         def backtrack(start, curr_sub):
-            
-            if tuple(curr_sub) not in curr_subset:
-                curr_subset.add(tuple(curr_sub))
-                subsets.append(curr_sub.copy())
             
             if start >= len(nums):
                 return
@@ -17,6 +12,7 @@ class Solution:
             
             for j in range(start, len(nums)):
                 curr_sub.append(nums[j])
+                subsets.append(curr_sub.copy())
                 backtrack(j+1, curr_sub)
                 curr_sub.pop()
                 
