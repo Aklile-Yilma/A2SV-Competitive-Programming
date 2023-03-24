1f46d4322895084e503aba6f1cbea959766676e4
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-03-24 20:02:00 +0300

    Time: 228 ms (30.33%), Space: 15.4 MB (82.50%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        
+        res = []
+        #cyclic sort
+        for ptr in range(len(nums)):
+            while nums[ptr] != ptr + 1:
+                correct_pos = nums[ptr] - 1
+                if nums[ptr] == nums[correct_pos]:
+                    break
+                    
+                nums[ptr], nums[correct_pos] = nums[correct_pos], nums[ptr] 
+                
+                
+        for idx in range(len(nums)):
+            if nums[idx] != idx + 1:
+                res.append(nums[idx])
+                res.append(idx + 1)
+            
+        return res
