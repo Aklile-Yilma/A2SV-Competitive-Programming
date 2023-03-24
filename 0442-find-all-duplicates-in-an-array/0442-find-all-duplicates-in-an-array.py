e667e13936f8045808d822f753bdcae9bb15e269
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-03-24 19:53:59 +0300

    Time: 410 ms (19.31%), Space: 21.1 MB (91.71%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        
+        res = []
+        #cyclic sort
+        for ptr in range(len(nums)):
+            while nums[ptr] != ptr + 1:
+                correct_pos = nums[ptr] - 1
+                if nums[ptr] == nums[correct_pos]:
+                    break
+                    
+                nums[ptr], nums[correct_pos] = nums[correct_pos], nums[ptr] 
+                
+                
+        for idx in range(len(nums)):
+            if nums[idx] != idx + 1:
+                res.append(nums[idx])
+            
+        return res
+        
