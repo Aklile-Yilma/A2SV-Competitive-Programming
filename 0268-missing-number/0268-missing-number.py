267d70269ba197f2685eb50cdcc5dc1e445baa60
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-03-30 10:29:18 +0300

    Time: 142 ms (45.65%), Space: 15.2 MB (28.49%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,21 +1,38 @@
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        # to get range of number from 0  to n
+        xor_nums = 0
+        for num in nums:
+            xor_nums ^= num
+            
+        xor_all = 0
+        for idx in range(len(nums)+1):
+            xor_all ^= idx
+            
+        return xor_nums ^ xor_all
+
+
+#using cyclic sort
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+        
+#         idx = 0
+    
+#         while idx < len(arr):
+#             if arr[idx] != idx and arr[idx] < len(nums):
+#                 corr_pos = arr[idx] 
+#                 arr[idx], arr[corr_pos] = arr[corr_pos], arr[idx]
+#             else:
+#                 idx += 1 
+                
+#         for idx in range(len(nums)):
+#             if idx != nums[idx]:
+#                 return idx
+        
+#         return len(nums)
+    
+    
+
         
         
-        for ptr in range(len(nums)):
-            
-            while nums[ptr] != ptr and nums[ptr] < len(nums):
-                
-                right = nums[ptr]
-                
-                nums[ptr], nums[right] = nums[right], nums[ptr]
-                
-            
-        for idx in range(len(nums)):
-            if nums[idx] != idx:
-                return idx
-            
-        return len(nums)
-                
