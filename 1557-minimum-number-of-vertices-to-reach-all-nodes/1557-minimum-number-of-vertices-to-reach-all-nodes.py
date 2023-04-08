e83a06d5bc8a4b1ba70d065822525b83158e5faa
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-04-08 14:42:30 +0300

    Time: 1148 ms (83.26%), Space: 52.2 MB (77.35%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -10,4 +10,27 @@ class Solution:
         for idx in range(n):
             if indegree[idx] == 0:
                 answer.append(idx)
+                
         return answer
+    
+    
+# class Solution:
+#     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        
+#         answer = []
+#         graph = defaultdict(list)
+        
+#         for u, v in edges:
+#             graph[u].append(v)
+        
+#         children = []
+#         for node in graph:
+#             children.extend(graph[node])
+        
+#         children = set(children)
+        
+#         for node in graph:
+#             if node not in children:
+#                 answer.append(node)
+                
+#         return answer
