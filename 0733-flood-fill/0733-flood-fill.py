a5e208006b68222bdd1c04d8530052dde9e44b74
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-04-10 13:56:52 +0300

    Time: 84 ms (21.11%), Space: 13.9 MB (98.66%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,31 +1,25 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         
-        row_len = len(image)
-        col_len = len(image[0])
         initial_color = image[sr][sc]
+        directions = [1, 0, -1, 0, 1]        
+        inbound = lambda row, col: 0 <= row <  len(image) and 0 <= col < len(image[0])
         
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        
-        def inbound(row, col):
+        q = deque()
+        if color != image[sr][sc]:
+            q.append((sr, sc))
             
-            return 0 <= row < row_len and 0 <= col < col_len
-        
-        def dfs(row, col):
+        while q:
+            size = len(q)
+            for idx in range(size):
+                row, col = q.popleft()
+                if not inbound(row, col) or image[row][col] != initial_color:
+                    continue
+                
+                image[row][col] = color
+                for idx in range(len(directions)-1):
+                    q.append((row + directions[idx], col + directions[idx+1]))
+                    
             
-            #visited this before
-            if image[row][col] == color:
-                return
-            
-            image[row][col] = color
-            for direction in directions:
-                new_row = row + direction[0]
-                new_col = col + direction[1]
-                if inbound(new_row, new_col) and image[new_row][new_col] == initial_color:
-                    dfs(new_row, new_col)
-            return
-        
-        dfs(sr, sc)
-        
         return image
-        
+                
