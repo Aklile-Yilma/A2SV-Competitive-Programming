b426f9b523dbb5ded0d683babe3d40dc5a1f4be9
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-04-11 01:33:09 +0300

    Time: 146 ms (47.94%), Space: 17.5 MB (40.81%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -5,25 +5,21 @@ class Solution:
         directions = [1, 0, -1, 0, 1]
         inbound = lambda row, col: 0 <= row < row_len and 0 <= col < col_len
         max_area = 0
-        count = 1
         
         def dfs(row, col):
-            nonlocal count 
-            
+
+            depth = 1
             grid[row][col] = 0
             for idx in range(len(directions)-1):
                 new_row, new_col = row + directions[idx], col + directions[idx+1]
                 if inbound(new_row, new_col) and grid[new_row][new_col] != 0:
-                    count += 1
-                    dfs(new_row, new_col)
+                    depth += dfs(new_row, new_col)
 
-            return 
+            return depth
         
         for row in range(row_len):
             for col in range(col_len):
                 if grid[row][col] != 0:
-                    dfs(row, col)
-                    max_area = max(max_area, count)
-                count = 1
+                    max_area = max(max_area, dfs(row, col))
                     
         return max_area
