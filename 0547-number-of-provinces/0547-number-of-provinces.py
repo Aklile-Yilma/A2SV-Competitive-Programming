5260a0674d040701f1d1181f3f8db03a74a02333
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-04-11 01:58:06 +0300

    Time: 723 ms (5.03%), Space: 24.8 MB (5.04%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -2,7 +2,6 @@ class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         
         row_len, col_len = len(isConnected), len(isConnected[0])
-        visited = set()
         inbound = lambda row, col: 0 <= row < row_len and 0 <= col < col_len
         count = 0
         
@@ -10,17 +9,17 @@ class Solution:
                 
             for col in range(col_len):
                 new_row, new_col = row, col
-                if inbound(row, col) and (new_row, new_col) not in visited and isConnected[row][col] != 0:
-                    visited.add((new_row, new_col))
+                if inbound(row, col) and  isConnected[row][col] != 0:
+                    isConnected[row][col] = 0
                     dfs(new_col, new_row)
             
             return
         
         for row in range(row_len):
             for col in range(col_len):
-                if (row,col) not in visited and isConnected[row][col] != 0:
-                    visited.add((row, col))
+                if isConnected[row][col] != 0:
                     count += 1
+                    isConnected[row][col] = 0
                     dfs(row, col)
                     
         return count
