5356b0f71e04df94d4bf66804c7593ba2949fae9
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-04-12 18:03:46 +0300

    Time: 714 ms (58.22%), Space: 22.4 MB (13.60%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,25 +1,29 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
         
-        colors_map = defaultdict(bool)
+        group1 = set()
+        group2 = set()
         visited = set()
         graph = defaultdict(list)
         
         for u, v in dislikes:
             graph[u].append(v)
-            graph[v].append(u)
-            colors_map[u], colors_map[v] = None, None
-            
+            graph[v].append(u)            
         
         def dfs(parent, curr_node, curr_color):
             
             if curr_node in visited:
-                if colors_map[curr_node] != curr_color:
+                if (curr_color and curr_node not in group1) or (not curr_color and curr_node not in group2):
                     return False
+                
                 return True
             
             visited.add(curr_node)
-            colors_map[curr_node] = curr_color
+            if curr_color: 
+                group1.add(curr_node)
+            else:
+                group2.add(curr_node)
+                
             for neighbor in graph[curr_node]:
                 if neighbor != parent:
                     possible = dfs(curr_node, neighbor, not curr_color)
