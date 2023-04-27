6c8c0a9e1a0df6624b2e19aa296505ca0d4282e5
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-04-27 19:40:15 +0300

    Time: 2407 ms (8.27%), Space: 17.8 MB (5.72%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -0,0 +1,41 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        deadends = set(deadends)
+        if "0000" in deadends:
+            return -1
+        
+        def get_neighbors(node):
+            node_list = list(node)
+            children_list = []
+            
+            for i in range(len(node)):
+                curr = node_list[i]
+                nxt = "0" if curr == '9' else str(int(curr) + 1)
+                children_list.append(''.join(node_list[:i] + [nxt] + node_list[i+1:]))
+                prev = "9" if curr == '0' else str(int(curr) - 1)
+                children_list.append(''.join(node_list[:i] + [prev] + node_list[i+1:]))
+        
+            return children_list
+        
+        q = deque()
+        q.append(('0000', 0))
+        visited = {'0000'}
+        min_count = float('inf')
+        
+        while q:
+            node, step = q.popleft()
+            if node == target:
+                min_count = min(step, min_count)
+            children = get_neighbors(node)
+            for child in children:
+                if child not in deadends and child not in visited:
+                    visited.add(child)
+                    q.append((child, step + 1))
+                    
+        return min_count if min_count != float('inf') else -1
+        
+        
+        
+
+        
+        
