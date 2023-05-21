27372d27d543f0f9760f366c6a9685c01bb812b1
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-05-21 17:43:06 +0300

    Time: 55 ms (47.14%), Space: 16.4 MB (37.71%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -6,31 +6,21 @@ class Solution:
         for l1, l2 in zip(s1, s2):
             uf.union(l1, l2)
         
-        collections = defaultdict(set)
-        for letter in uf.root:
-            par = uf.find(letter)
-            collections[par].add(letter)
-        
-        # assign the smallest letter in connected letters
+        # assign the smallest letter in connected letters which is the root 
         ans = list(baseStr)
         for idx in range(len(baseStr)):
-            curr_letter = baseStr[idx]
-            for key, values in collections.items():
-                if curr_letter in values:
-                    for v in values:
-                        if v < ans[idx]:
-                            ans[idx] = v
-                            
+            letter = baseStr[idx]
+            par = uf.find(letter)
+            ans[idx] = par
+            
         return ''.join(ans)
                             
 class UnionFind:
     def __init__(self):
         self.root = {}
-        self.size = {}
         
         for idx in range(ord('a'), ord('z')+1):
             self.root[chr(idx)] = chr(idx)
-            self.size[chr(idx)] = 1
             
     def find(self, x):
         # hold x
@@ -51,18 +41,14 @@ class UnionFind:
     def union(self, x, y):
         root1 = self.find(x)
         root2 = self.find(y)
-        size1 = self.size[root1]
-        size2 = self.size[root2]
         
         if root1 == root2:
             return
 
-        if size1 <= size2:
+        if root1 <= root2:
             self.root[root2] = root1
-            self.size[root1] += self.size[root2]
         else:
             self.root[root1] = root2
-            self.size[root2] += self.size[root1]
 
     def connected(self, x, y):
         return self.find(x) == self.find(y)
