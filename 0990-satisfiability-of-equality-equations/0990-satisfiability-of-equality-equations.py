352604380a69e8a28fbacb445152acf5f54d8a99
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-05-21 16:23:44 +0300

    Time: 66 ms (15.74%), Space: 16.6 MB (16.26%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -1,40 +1,33 @@
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
         
-        uf = UnionFind(len(equations)*2)
-        letter_map = {}
-        idx = 0
-        
+        uf = UnionFind(len(equations)*2)        
         for eq in equations:
             letter1 = eq[0]
             symbol = eq[1:3]
             letter2 = eq[3]
             
-            if letter1 not in letter_map:
-                letter_map[letter1] = idx
-                idx += 1
-            if letter2 not in letter_map:
-                letter_map[letter2] = idx
-                idx += 1
             if symbol == "==":
-                uf.union(letter_map[letter1], letter_map[letter2])
+                uf.union(letter1, letter2)
               
         for eq in equations:
             letter1 = eq[0]
             symbol = eq[1:3]
             letter2 = eq[3]
-            
-            if symbol == "!=" and uf.connected(letter_map[letter1], letter_map[letter2]):
+            if symbol == "!=" and uf.connected(letter1, letter2):
                 return False
             
         return True
                 
 class UnionFind:
     def __init__(self, n):
-        self.n = 0
-        self.root = [node for node in range(n)]
-        self.size = [1 for _ in range(n)]
-
+        self.root = {}
+        self.size = {}
+        
+        for idx in range(ord('a'), ord('z')+1):
+            self.root[chr(idx)] = chr(idx)
+            self.size[chr(idx)] = 1
+            
     def find(self, x):
         # hold x
         hold = x
@@ -59,10 +52,10 @@ class UnionFind:
 
         if size1 <= size2:
             self.root[root2] = root1
-            self.size[root1] += root2
+            self.size[root1] += self.size[root2]
         else:
             self.root[root1] = root2
-            self.size[root2] += root1
+            self.size[root2] += self.size[root1]
 
     def connected(self, x, y):
         return self.find(x) == self.find(y)
