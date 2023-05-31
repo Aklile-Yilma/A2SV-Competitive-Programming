08dc648cc68244ea2d618afe08536a5ed38cc57f
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-05-31 10:50:11 +0300

    Time: 54 ms (11.04%), Space: 16.6 MB (18.07%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -7,7 +7,6 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         
-        
         def dfs(curr_idx, canPickLast):
             if curr_idx == len(nums)-1 and not canPickLast:
                 return 0
@@ -16,23 +15,16 @@ class Solution:
                 return 0
             
             if (curr_idx, canPickLast) not in self.memo:
-                if curr_idx == 0:
-                    #pick 
-                    pick = dfs(curr_idx + 2, False) + nums[curr_idx]
-                    #not_pick
-                    n_pick = dfs(curr_idx+1, True)
-                else:
-                    #pick 
-                    pick = dfs(curr_idx + 2, canPickLast) + nums[curr_idx]
-                    #not_pick
-                    n_pick = dfs(curr_idx+1, canPickLast)
-                
+                #pick 
+                pick = dfs(curr_idx + 2, canPickLast) + nums[curr_idx]
+                #not_pick
+                n_pick = dfs(curr_idx+1, canPickLast)
                 #find max
                 self.memo[(curr_idx, canPickLast)] = max(pick, n_pick)
                 
             return self.memo[(curr_idx, canPickLast)]
         
-        return dfs(0, None)
+        return max(dfs(2, False) + nums[0], dfs(1, True))
                     
                     
                 
