5aca4e165e55eda3ad657b14021dfc7f28bc8436
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-06-05 15:24:33 +0300

    Time: 91 ms (13.53%), Space: 17.8 MB (13.33%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,14 +1,34 @@
 class Solution:        
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        row = len(triangle)
         
-        #top-down
-        @lru_cache(None)
-        def dfs(curr_row, curr_col):
+        dp = []
+        curr_col = 1
+        for _ in range(row):
+            dp.append([float('inf')] * curr_col)
+            curr_col += 1
             
-            if curr_row == len(triangle):
-                return 0 
+        dp[0][0] = triangle[0][0] 
+        
+        # push dp
+        for r in range(row-1):
+            for c in range(len(triangle[r])):
+                dp[r+1][c] = min(dp[r+1][c], dp[r][c] + triangle[r+1][c])
+                dp[r+1][c+1] = min(dp[r+1][c+1], dp[r][c] + triangle[r+1][c+1])
+                
+        return min(dp[-1])
+    
+# class Solution:        
+#     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        
+#         #top-down
+#         @lru_cache(None)
+#         def dfs(curr_row, curr_col):
             
-            return triangle[curr_row][curr_col] + min(dfs(curr_row + 1, curr_col), dfs(curr_row+1, curr_col + 1))
+#             if curr_row == len(triangle):
+#                 return 0 
+            
+#             return triangle[curr_row][curr_col] + min(dfs(curr_row + 1, curr_col), dfs(curr_row+1, curr_col + 1))
                        
-        return dfs(0, 0)
+#         return dfs(0, 0)
                 
