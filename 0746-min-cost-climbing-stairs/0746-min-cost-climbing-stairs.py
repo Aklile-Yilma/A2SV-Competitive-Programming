a222608e9a1d35b8d895fb501c57569072c8df21
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-06-08 14:36:00 +0300

    Time: 82 ms (10.36%), Space: 16.4 MB (40.80%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,15 +1,30 @@
+# class Solution:
+#     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+#         self.memo = {}
+#         def recursion(idx):
+#             nonlocal cost
+#             if idx == len(cost)-1 or idx == len(cost)-2:
+#                 return cost[idx]
+            
+#             if idx not in self.memo:
+#                 self.memo[idx] = [recursion(idx+1), recursion(idx+2)]
+                
+#             return cost[idx] + min(self.memo[idx])
+            
+#         return min(recursion(0), recursion(1))
+    
+    
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         
-        self.memo = {}
-        def recursion(idx):
-            nonlocal cost
-            if idx == len(cost)-1 or idx == len(cost)-2:
-                return cost[idx]
+        size = len(cost) + 1
+        dp = [float('inf')] * size
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        cost.append(0)
+        
+        for i in range(2, size):
+            dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
             
-            if idx not in self.memo:
-                self.memo[idx] = [recursion(idx+1), recursion(idx+2)]
-                
-            return cost[idx] + min(self.memo[idx])
-            
-        return min(recursion(0), recursion(1))
+        return dp[size-1]
