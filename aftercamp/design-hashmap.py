12cb7b11869b44e4cf0f16cfc00aae81049fef3c
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2024-01-03 00:34:46 +0300

    Added solution for Design HashMap

---
+++ aftercamp/design-hashmap.py
@@ -0,0 +1,22 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.map = {}
+
+    def put(self, key: int, value: int) -> None:
+        self.map[key] = value
+
+    def get(self, key: int) -> int:
+        return self.map.get(key, -1)
+
+    def remove(self, key: int) -> None:
+        if self.map.get(key, -1) != -1:
+            del self.map[key]
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
